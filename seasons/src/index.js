fd3c35be3c6a19ff53a753d2f76cab0a723fb9ea
Raton Biswas <32263412+RatonBiswas@users.noreply.github.com>
2021-02-13 12:42:53 +0600

    State in React Components

---
+++ seasons/src/index.js
@@ -0,0 +1,36 @@
+import ReactDOM from 'react-dom';
+
+import React, { Component } from 'react';
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      lat:null,
+      errorMessage: ''
+    }
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({lat:position.coords.latitude})
+      },
+      err => {
+        this.setState({errorMessage: err.message})
+      }
+    )
+  }
+  render() {
+    if(this.state.errorMessage && !this.state.lat){
+      return <div>Error: {this.state.errorMessage}</div>
+    }
+    if(!this.state.errorMessage && this.state.lat){
+      return <div>Lattitude: {this.state.lat}</div>
+    }
+    return <div>Loading...</div>
+  }
+}
+
+ReactDOM.render(
+  <App />,
+document.querySelector('#root')
+);
+
