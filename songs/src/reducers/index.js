e50f07a142b4098ee1c899b6f0b6f90dd881b9e2
Raton Biswas <32263412+RatonBiswas@users.noreply.github.com>
2021-03-10 20:10:36 +0600

    Wiring Up the Provider

---
+++ songs/src/reducers/index.js
@@ -0,0 +1,23 @@
+import {combineReducers} from 'redux'
+
+
+const songReducer = () =>{
+    return [
+        {title: 'No Scrubs',duration:'4.05'},
+        {title: 'Macarena',duration:'2.30'},
+        {title: 'All Star',duration:'3.15'},
+        {title: 'I Want it That Way',duration:'1.45'}
+    ];
+};
+
+const selectedSongReducer = (selecedSong=null,action)=>{
+    if(action.type === 'SONG_SELECTED'){
+            return action.payload
+    }
+    return selecedSong;
+}
+
+export default combineReducers({
+    song: songReducer,
+    selectedSong: selectedSongReducer
+})
