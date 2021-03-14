2e980f93211e3db916ac200066a8753ec4a9070d
Raton Biswas <32263412+RatonBiswas@users.noreply.github.com>
2021-03-15 00:07:42 +0600

    Initial App Setup

--- songs/src/components/SongDetails.js
+++ songs/src/components/SongDetails.js
@@ -1,16 +1,24 @@
-import React from 'react';
-import {connect} from 'react-redux'
+import React from "react";
+import { connect } from "react-redux";
 
-const SongDetails = () => {
-    return (
-        <div>
-            Song Details
-        </div>
-    );
+const SongDetails = ({ song }) => {
+  if (!song) {
+    return <div>Select A Song</div>;
+  }
+  return (
+    <div>
+      <h3>Details for : </h3>
+      <p>
+        Teacher : {song.title}
+        <br/>
+        Duration : {song.duration}
+      </p>
+    </div>
+  );
 };
 
 const mapStateToProps = (state) => {
-    return {song: state.selectedSong}
-}
+  return { song: state.selectedSong };
+};
 
 export default connect(mapStateToProps)(SongDetails);
