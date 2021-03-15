4a011e5e6a5d07906b8fe564d4f10d396dc008e6
Raton Biswas <32263412+RatonBiswas@users.noreply.github.com>
2021-03-15 13:52:24 +0600

    Avoid bad approach code , to avoid use redux-thunk

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,6 +1,7 @@
 import jsonPlaceholder from '../apis/jsonPlaceholder'
 
 export const fetchPosts = async () =>{
+    // bad approach because we found Error: Actions must be plain objects. Use custom middleware for async actions.
     const response = await jsonPlaceholder.get('/posts')
     return {
         type : 'FETCH_POSTS',
