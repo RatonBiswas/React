907b46e46831a11fe599b56b14005c33cc9bcf22
Raton Biswas <32263412+RatonBiswas@users.noreply.github.com>
2021-03-16 01:24:43 +0600

    Shortened Syntax with Redux Thunk.

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,10 +1,11 @@
-import jsonPlaceholder from '../apis/jsonPlaceholder'
+import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPosts = async () =>{
-    // bad approach because we found Error: Actions must be plain objects. Use custom middleware for async actions.
-    const response = await jsonPlaceholder.get('/posts')
-    return {
-        type : 'FETCH_POSTS',
-        payload: response
-    }
-}
+export const fetchPosts = () => async (dispatch) =>
+  {
+    const response = jsonPlaceholder.get("/posts");
+
+    await dispatch({
+      type: "FETCH_POSTS",
+      payload: response,
+    });
+  };
