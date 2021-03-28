ac2a6f2e4bf8f5f2d6e3880f48f157fabba9ddbc
Raton Biswas <32263412+RatonBiswas@users.noreply.github.com>
2021-03-28 17:56:49 +0600

    One Time Memoization

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -1,19 +1,33 @@
+/* eslint-disable no-unused-vars */
+import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPosts = () => async dispatch =>
-  {
-    const response = await jsonPlaceholder.get("/posts");
+export const fetchPosts = () => async (dispatch) => {
+  const response = await jsonPlaceholder.get("/posts");
 
-    dispatch({
-      type: 'FETCH_POSTS',
-      payload: response.data,
-    })
-  }
+  dispatch({
+    type: "FETCH_POSTS",
+    payload: response.data,
+  });
+};
 
-  export const fetchUser = (id)=> async dispatch =>{
-    const response = await jsonPlaceholder.get(`/users/${id}`)
-    dispatch({
-      type: 'FETCH_USER',
-      payload: response.data 
-    })
-  }
+export const fetchUser = id=>  dispatch =>{
+  _fetchUser(id,dispatch);
+}
+
+const _fetchUser = _.memoize(async(id,dispatch)=>{
+  const response = await jsonPlaceholder.get(`/users/${id}`)
+  dispatch({
+    type: 'FETCH_USER',
+    payload: response.data
+  })
+})
+// export const fetchUser = function (id) {
+//   return _.memoize(async function (dispatch) {
+//     const response = await jsonPlaceholder.get(`/users/${id}`);
+//     dispatch({
+//       type: "FETCH_USER",
+//       payload: response.data,
+//     });
+//   });
+// };
