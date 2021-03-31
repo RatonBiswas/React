4e1e70e5a045b67e6d4f1aa756e6bbba95e5d1c1
Raton Biswas <32263412+RatonBiswas@users.noreply.github.com>
2021-03-31 18:11:57 +0600

    Action Creators in Action Creators!

--- blog/src/actions/index.js
+++ blog/src/actions/index.js
@@ -2,6 +2,13 @@
 import _ from "lodash";
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
+export const fetchPostsAndUsers = () => async (dispatch,getState) =>{
+  console.log('about to fetch post');
+  await dispatch(fetchPosts())
+  // console.log(getState().posts);
+  _.map(getState().posts,'userId')
+}
+
 export const fetchPosts = () => async (dispatch) => {
   const response = await jsonPlaceholder.get("/posts");
 
@@ -11,17 +18,22 @@ export const fetchPosts = () => async (dispatch) => {
   });
 };
 
-export const fetchUser = id=>  dispatch =>{
-  _fetchUser(id,dispatch);
-}
-
-const _fetchUser = _.memoize(async(id,dispatch)=>{
+export const fetchUser = id=>  async dispatch =>{
   const response = await jsonPlaceholder.get(`/users/${id}`)
   dispatch({
     type: 'FETCH_USER',
     payload: response.data
   })
-})
+}
+
+// const _fetchUser = _.memoize(async(id,dispatch)=>{
+  // const response = await jsonPlaceholder.get(`/users/${id}`)
+  // dispatch({
+  //   type: 'FETCH_USER',
+  //   payload: response.data
+  // })
+// })
+
 // export const fetchUser = function (id) {
 //   return _.memoize(async function (dispatch) {
 //     const response = await jsonPlaceholder.get(`/users/${id}`);
