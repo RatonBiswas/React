e25342c3f67786e7e9616f33db434ef6164da01d
Raton Biswas <32263412+RatonBiswas@users.noreply.github.com>
2021-04-02 12:13:58 +0600

    Helper Functions in Function Components

--- widget/src/components/Accordion.js
+++ widget/src/components/Accordion.js
@@ -1,18 +1,20 @@
 import React from 'react';
 
 const Accordion = ({items}) => {
-    const renderItem = items.map(item=>{
-        return <div>
-            <div className="title active">
+    const titleClicked = (index)=>{
+        console.log('Title Clicked', index)
+    }
+    const renderItem = items.map((item,index) =>{
+        return <React.Fragment key={item.title}>
+            <div className="title active" onClick={()=>titleClicked(index)}>
                 <i className="dropdown icon"></i>
-                <h2>{item.title}</h2>
-                <p>{item.content}</p>
-                
+                {item.title}
             </div>
-        </div>
+            <div className="content active">{item.content}</div>
+        </React.Fragment>
     })
     return (
-        <div>
+        <div className="ui styled accordion">
            {renderItem}
         </div>
     );
