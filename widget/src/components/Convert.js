a73cc1be36ab0d03e0c9a569f5b374d872d9c587
Raton Biswas <32263412+RatonBiswas@users.noreply.github.com>
2021-04-29 11:50:04 +0600

    Debouncing Translation Updates

--- widget/src/components/Convert.js
+++ widget/src/components/Convert.js
@@ -2,11 +2,20 @@ import React,{ useState, useEffect} from 'react';
 import axios from 'axios';
 const Convert = ({language,text}) => {
     const [translated,setTranslation] = useState('')
+    const [debouncedText , setDebouncedText] = useState(text)
+    useEffect(() =>{
+        const timerId = setTimeout(()=>{ 
+            setDebouncedText(text);
+        },500)
+        return () => {
+            clearTimeout(timerId)
+        }
+    },[text])
     useEffect(() =>{
         const doTranslated = async () =>{
             const {data} = await axios.post('https://translation.googleapis.com/language/translate/v2',{},{
                 params:{
-                    q: text,
+                    q: debouncedText,
                     target: language.value,
                     key: "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM"
                 }
@@ -14,7 +23,7 @@ const Convert = ({language,text}) => {
             setTranslation(data.data.translations[0].translatedText)
         }
         doTranslated()
-    },[language,text])
+    },[language,debouncedText])
     return (
         <div>
             <h1 className="ui header">{translated}</h1>
