a750f56f1b96e8b9864fa1da71d83eb0cc791326
Raton Biswas <32263412+RatonBiswas@users.noreply.github.com>
2021-04-29 11:33:50 +0600

    Displaying Translated Text

--- widget/src/components/Convert.js
+++ widget/src/components/Convert.js
@@ -1,18 +1,23 @@
-import React,{  useEffect} from 'react';
+import React,{ useState, useEffect} from 'react';
 import axios from 'axios';
 const Convert = ({language,text}) => {
+    const [translated,setTranslation] = useState('')
     useEffect(() =>{
-       const response =  axios.post('https://translation.googleapis.com/language/translate/v2',{},{
-            params:{
-                q: text,
-                target: language.value,
-                key: "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM"
-            }
-        })
+        const doTranslated = async () =>{
+            const {data} = await axios.post('https://translation.googleapis.com/language/translate/v2',{},{
+                params:{
+                    q: text,
+                    target: language.value,
+                    key: "AIzaSyCHUCmpR7cT_yDFHC98CZJy2LTms-IwDlM"
+                }
+            })
+            setTranslation(data.data.translations[0].translatedText)
+        }
+        doTranslated()
     },[language,text])
     return (
         <div>
-            {this.response}
+            <h1 className="ui header">{translated}</h1>
         </div>
     );
 };
