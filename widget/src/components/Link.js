d0cbcf45f3ce2ca08c80f605cf2e880232c8eb02
Raton Biswas <32263412+RatonBiswas@users.noreply.github.com>
2021-05-03 12:13:16 +0600

    Handling Command Clicks

--- widget/src/components/Link.js
+++ widget/src/components/Link.js
@@ -2,6 +2,10 @@ import React from 'react';
 
 const Link = ({ className, href, children }) => {
   const onClick = (event) => {
+    if(event.metakey|| event.ctrlKey){
+      return;
+    }
+    
     event.preventDefault();
     window.history.pushState({}, '', href);
 
