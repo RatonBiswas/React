1570d9fc49e8271361162bc900f504bb994b4352
Raton Biswas <32263412+RatonBiswas@users.noreply.github.com>
2021-05-08 12:19:44 +0600

    Creating OAuth Credentials

--- stream/client/src/components/App.js
+++ stream/client/src/components/App.js
@@ -1,33 +1,24 @@
 /* eslint-disable no-unreachable */
 import React from "react";
-import { BrowserRouter, Route, Link } from "react-router-dom";
-
-const PageOne = () => {
-  return (
-    <div>
-      PageOne
-      <Link to="/pagetwo">Reach to PageTwo</Link>
-    </div>
-  );
-};
-
-const PageTwo = () => {
-  return (
-    <div>
-      PageTwo
-      <button>Click me</button>
-      <Link to="/">Reach to PageOne</Link>
-    </div>
-  );
-};
+import { BrowserRouter, Route } from "react-router-dom";
+import StreamEdit from './streams/StreamEdit'
+import StreamCreate from './streams/StreamCreate'
+import StreamShow from './streams/StreamShow'
+import StreamDelete from './streams/StreamDelete'
+import StreamList from './streams/StreamList'
+import Header from './Header';
 
 const App = () => {
   return (
     <div>
       <BrowserRouter>
         <div>
-          <Route path="/" exact component={PageOne} />
-          <Route path="/pagetwo" component={PageTwo} />
+        <Header />
+          <Route path="/" exact component={StreamList} />
+          <Route path="/streams/new" exact component={StreamCreate} />
+          <Route path="/streams/edit" exact component={StreamEdit} />
+          <Route path="/streams/delete" exact component={StreamDelete} />
+          <Route path="/streams/show" exact component={StreamShow} />
         </div>
       </BrowserRouter>
     </div>
