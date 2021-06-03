95bec966ad54f1506725b24100984004f8006bbf
Raton Biswas <32263412+RatonBiswas@users.noreply.github.com>
2021-06-03 13:54:22 +0600

    Conditionally showing stream details

--- stream/client/src/components/streams/StreamDelete.js
+++ stream/client/src/components/streams/StreamDelete.js
@@ -1,24 +1,32 @@
-import React from 'react';
-import Modal from '../Modal';
-import history from '../../history'
-const StreamDelete = () => {
-    const actions = (
-        <React.Fragment>
-            <button className="ui primary button">Delete</button>
-            <button className="ui button">Cancel</button>
-        </React.Fragment>
-    )
-  return (
-    <div>
-      StreamDelete
-      <Modal 
-        title= "Delete Stream"
-        content="Are you sure you want to delete this stream?"
-        actions={actions}
-        onDismiss={()=>history.push('/')}
-      />
-    </div>
-  );
-};
+import React, { Component } from "react";
+import Modal from "../Modal";
+import {connect} from 'react-redux'
+import history from '../../history';
+import { fetchStream } from '../../actions'
 
-export default StreamDelete;
+class StreamDelete extends Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id)
+  }
+  renderAction() {
+    <React.Fragment>
+      <button className="ui primary button">Delete</button>
+      <button className="ui button">Cancel</button>
+    </React.Fragment>;
+  } 
+  render() {
+    return (
+      <div>
+        StreamDelete
+        <Modal
+          title="Delete Stream"
+          content="Are you sure you want to delete this stream?"
+          actions={this.renderAction()}
+          onDismiss={() => history.push("/")}
+        />
+      </div>
+    );
+  }
+}
+
+export default connect(null,{fetchStream})(StreamDelete);
