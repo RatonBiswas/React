6a19e638d1ac54a2fd2c130a65113e528834fd83
Raton Biswas <32263412+RatonBiswas@users.noreply.github.com>
2021-07-03 11:26:37 +0600

    Pulling from multiple context

--- translate/src/components/App.js
+++ translate/src/components/App.js
@@ -1,24 +1,33 @@
 import React, { Component } from "react";
-import UserCreate from "./UserCreate"
-import LanguageContext from '../contexts/LanguageContext';
+import UserCreate from "./UserCreate";
+import LanguageContext from "../contexts/LanguageContext";
+import ColorContext from "../contexts/ColorContext";
 
 class App extends Component {
-  state = { language: 'english' }
+  state = { language: "english" };
 
-  onLanguageChange = language =>{
-      this.setState({ language})
-  }
+  onLanguageChange = (language) => {
+    this.setState({ language });
+  };
   render() {
     return (
       <div className="ui container">
         <div>
-          Select A Language : 
-          <i className="flag us" onClick = {()=> this.onLanguageChange('english')}></i>
-          <i className="flag bd" onClick = {()=> this.onLanguageChange('bangla')}></i>
+          Select A Language :
+          <i
+            className="flag us"
+            onClick={() => this.onLanguageChange("english")}
+          ></i>
+          <i
+            className="flag bd"
+            onClick={() => this.onLanguageChange("bangla")}
+          ></i>
         </div>
-        <LanguageContext.Provider value={this.state.language}>
-          <UserCreate/>
-        </LanguageContext.Provider>
+        <ColorContext.Provider value='primary'>
+          <LanguageContext.Provider value={this.state.language}>
+            <UserCreate />
+          </LanguageContext.Provider>
+        </ColorContext.Provider>
       </div>
     );
   }
--- translate/src/components/Button.js
+++ translate/src/components/Button.js
@@ -1,16 +1,28 @@
 import React, { Component } from "react";
 import LanguageContext from "../contexts/LanguageContext";
+import ColorContext from "../contexts/ColorContext";
 
 class Button extends Component {
   static contextType = LanguageContext;
+  renderSubmit(value) {
+    return value === "english" ? "Submit" : "জমা দিন"
+  }
+
+  renderButton(color) {
+    return (
+      <button className={`ui button ${color}`}>
+        <LanguageContext.Consumer>
+          {(value) => this.renderSubmit(value)}
+        </LanguageContext.Consumer>
+      </button>
+    )
+  }
   render() {
     // const text = this.context === "english" ? "Submit" : "জমা দিন";
     return (
-      <button className="ui button primary">
-        <LanguageContext.Consumer>
-          {(value) => (value === "english" ? "Submit" : "জমা দিন")}
-        </LanguageContext.Consumer>
-      </button>
+      <ColorContext.Consumer>
+        {(color) => this.renderButton(color)}
+      </ColorContext.Consumer>
     );
   }
 }
