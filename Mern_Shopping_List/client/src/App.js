34cd96290fdce99c193f7aa826d4420f33bd26ad
Raton Biswas <32263412+RatonBiswas@users.noreply.github.com>
2021-07-12 13:45:31 +0600

    Add item and remove item

--- Mern_Shopping_List/client/src/App.js
+++ Mern_Shopping_List/client/src/App.js
@@ -2,11 +2,12 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 import AppNavbar from './components/AppNavbar'
-
+import ShoppingList from './components/ShoppingList';
 function App() {
   return (
     <div className="App">
      <AppNavbar/>
+     <ShoppingList/>
     </div>
   );
 }
---
+++ Mern_Shopping_List/client/src/components/ShoppingList.js
@@ -0,0 +1,60 @@
+import React, { Component } from "react";
+import { Container, ListGroup, ListGroupItem, Button } from "reactstrap";
+import { CSSTransition, TransitionGroup } from "react-transition-group";
+import {v4 as uuid} from 'uuid'
+
+class ShoppingList extends Component {
+  state = {
+    items: [
+      { id: uuid(), name: "Eggs" },
+      { id: uuid(), name: "Milk" },
+      { id: uuid(), name: "Rice" },
+      { id: uuid(), name: "Oil" },
+    ],
+  };
+  render() {
+      const {items} = this.state
+    return (
+      <Container>
+        <Button
+          color="dark"
+          style={{ marginBottom: "2rem" }}
+          onClick={() => {
+            const name = prompt("Enter Item");
+            if (name) {
+              this.setState(state => ({
+                items: [...state.items, { id: uuid(), name }],
+              }));
+            }
+          }}
+        >Add Item
+        </Button>
+
+        <ListGroup>
+            <TransitionGroup className="shopping-list">
+                {items.map(({id,name})=>(
+                    <CSSTransition key={id} timeout={500} className="dark">
+                        <ListGroupItem>
+                            <Button 
+                                className="remove-btn"
+                                color="danger"
+                                size="sm"
+                                onClick={()=>{
+                                    this.setState(state=>({
+                                        items: state.items.filter(item=> item.id !== id)
+                                    }))
+                                }}>
+                                &times;
+                            </Button>
+                            {name}
+                        </ListGroupItem>
+                    </CSSTransition>
+                ))}
+            </TransitionGroup>
+        </ListGroup>
+      </Container>
+    );
+  }
+}
+
+export default ShoppingList;
