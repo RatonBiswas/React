7dbfa9677c1eca0d7d5f9fc42dbd3f6578da09a0
Raton Biswas <32263412+RatonBiswas@users.noreply.github.com>
2021-07-13 13:07:07 +0600

    Setup redux

--- Mern_Shopping_List/client/src/App.js
+++ Mern_Shopping_List/client/src/App.js
@@ -1,14 +1,19 @@
-import './App.css';
-import 'bootstrap/dist/css/bootstrap.min.css';
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
-import AppNavbar from './components/AppNavbar'
-import ShoppingList from './components/ShoppingList';
+import AppNavbar from "./components/AppNavbar";
+import ShoppingList from "./components/ShoppingList";
+import { Provider } from "react-redux";
+import store from "./store";
+
 function App() {
   return (
-    <div className="App">
-     <AppNavbar/>
-     <ShoppingList/>
-    </div>
+    <Provider store={store}>
+      <div className="App">
+        <AppNavbar />
+        <ShoppingList />
+      </div>
+    </Provider>
   );
 }
 
