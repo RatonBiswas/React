4d3902ed112bb0a3e52bfa62979a7997b2a50895
Raton Biswas <raton35-2241@diu.edu.bd>
2023-07-19 22:35:46 +0600

    simple Review app using react

---
+++ reviews/src/App.jsx
@@ -0,0 +1,16 @@
+import Review from "./Review";
+function App() {
+  return (
+    <main>
+      <section className="container">
+        <div className="title">
+          <h2>Our Reviews</h2>
+          <div className="title-underline"></div>
+        <Review/>
+        </div>
+      </section>
+    </main>
+  );
+}
+
+export default App;
